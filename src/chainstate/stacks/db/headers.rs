352759516c93539004ed9b35030beb753d80e40e
gregorycoppola <greg@hiro.so>
2022-02-09 16:54:05 +0000

    error is now deliberate panic

--- src/chainstate/stacks/db/headers.rs
+++ src/chainstate/stacks/db/headers.rs
@@ -204,7 +204,7 @@ impl StacksChainState {
                     parent_block_id, \
                     miner_signatures \
                     ) \
-                    VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19, ?20, ?21)", args)
+                    VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19, ?20, ?21, ?22)", args)
             .map_err(|e| Error::DBError(db_error::SqliteError(e)))?;
 
         Ok(())
