9472ba0f0d6bb7473fb8f27edc862aad19800d76
gregorycoppola <greg@hiro.so>
2022-02-11 18:20:37 +0000

    fixed stacks_db_staging_block_load_store_accept

--- src/chainstate/stacks/db/blocks.rs
+++ src/chainstate/stacks/db/blocks.rs
@@ -5721,7 +5721,7 @@ pub mod test {
             &proof,
             &TrieHash([2u8; 32]),
             &mblock_pubkey_hash,
-            &MessageSignatureList::from_single(MessageSignature([0x0cu8; 65])),
+            &MessageSignatureList::empty(),
         );
         block.header.version = 0x24;
         block
