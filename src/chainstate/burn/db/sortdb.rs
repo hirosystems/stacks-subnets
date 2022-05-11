63c591f4e9ad7e36ec38240293c8d43db7eb64ba
Greg Coppola <greg@hiro.so>
2022-05-11 11:37:38 -0500

    move

--- src/chainstate/burn/db/sortdb.rs
+++ src/chainstate/burn/db/sortdb.rs
@@ -3171,7 +3171,7 @@ impl<'a> SortitionHandleTx<'a> {
         assert!(snapshot.block_height < BLOCK_HEIGHT_MAX);
         assert!(snapshot.num_sortitions < BLOCK_HEIGHT_MAX);
 
-        info!(
+        test_debug!(
             "Insert block snapshot state {} for block {} ({},{}) {}",
             snapshot.index_root,
             snapshot.block_height,
