e2e697a3b93151388d7e06ae4edaff44da357f5a
Greg Coppola <greg@hiro.so>
2022-06-09 18:08:56 -0500

    tests compiling

--- testnet/stacks-node/src/config.rs
+++ testnet/stacks-node/src/config.rs
@@ -437,8 +437,7 @@ impl Config {
                 }
 
                 BurnchainConfig {
-                    chain: burnchain.chain.unwrap_or(default_burnchain_config.chain),
-                    chain_id: burnchain.chain_id.unwrap_or(default_burnchain_config.chain),
+                    chain_id: burnchain.chain_id,
                     observer_port: burnchain
                         .observer_port
                         .unwrap_or(default_burnchain_config.observer_port),
@@ -1070,7 +1069,7 @@ impl BurnchainConfig {
 #[derive(Clone, Deserialize, Default)]
 pub struct BurnchainConfigFile {
     /// String-valued unique identifier, e.g., "mainnet", "testnet".
-    pub chain: Option<String>,
+    pub chain_id: u32,
     pub burn_fee_cap: Option<u64>,
     pub observer_port: Option<u16>,
     pub mode: Option<String>,
