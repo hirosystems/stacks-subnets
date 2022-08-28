16cef3ffd3d87f169413f3e7aee06691276426d8
Greg Coppola <greg@hiro.so>
2022-08-28 14:13:55 -0500

    rename the conf file

--- testnet/stacks-node/run.sh
+++ testnet/stacks-node/run.sh
@@ -1,10 +1,6 @@
-rm -rf ~/hc-space/*
-
-export STACKS_FILTER="DEBG|trie_sql.rs|node.rs|storage.rs|marf.rs|cache.rs|p2p.rs|inv.rs|prune.rs|trie.rs|db.rs|neighbors.rs|processing.rs|burn.mod.rs|blocks.rs"
-
-# cargo build --bin hyperchain-node --release && ../../target/release/hyperchain-node start --config conf/aaron-conf.toml 2>&1 | ~/tracer/bin/filter-tracer.sh
+# rm -rf ~/hc-space/*
 
 export BLOCKSTACK_DEBUG=0
 
-cargo build --bin hyperchain-node --release && \
-	../../target/release/hyperchain-node start --config conf/greg-conf.toml 2>&1 | ~/tracer/bin/trace-l2.sh
+# cargo build --bin hyperchain-node --release && \
+	../../target/release/hyperchain-node start --config conf/l2-miner-profiling.toml 2>&1 | ~/tracer/bin/trace-l2.sh
