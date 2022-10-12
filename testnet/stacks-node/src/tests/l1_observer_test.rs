9dc80f6f61738384662d5eacf25022c5ffe49084
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2022-10-12 11:01:59 +0530

    updated comment

--- testnet/stacks-node/src/tests/l1_observer_test.rs
+++ testnet/stacks-node/src/tests/l1_observer_test.rs
@@ -260,8 +260,8 @@ fn select_transactions_where(
     return result;
 }
 
-/// Deserializes the `StacksTransaction` objects from `blocks` and returns all those that
-/// match `test_fn`.
+/// Deserializes the `BlockstackOperationType` objects (non-null objects are produced by burn 
+/// transactions) from `blocks` and returns all those that match `test_fn`.
 fn select_burn_transactions_where(
     blocks: &Vec<serde_json::Value>,
     test_fn: fn(&BlockstackOperationType) -> bool,
