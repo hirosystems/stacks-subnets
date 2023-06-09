cfc04a7fd22f9bbb7467b68eb3c4128b86567bfa
Brice Dobry <brice@hiro.so>
2023-06-09 12:28:15 -0400

    fix: remove schema 3 from DB

--- src/chainstate/stacks/db/mod.rs
+++ src/chainstate/stacks/db/mod.rs
@@ -193,19 +193,12 @@ impl DBConfig {
     pub fn supports_epoch(&self, epoch_id: StacksEpochId) -> bool {
         match epoch_id {
             StacksEpochId::Epoch10 => true,
-            StacksEpochId::Epoch20 => {
-                self.version == "1"
-                    || self.version == "2"
-                    || self.version == "3"
-                    || self.version == "4"
-            }
-            StacksEpochId::Epoch2_05 => {
-                self.version == "2" || self.version == "3" || self.version == "4"
-            }
-            StacksEpochId::Epoch21 => self.version == "3" || self.version == "4",
-            StacksEpochId::Epoch22 => self.version == "3" || self.version == "4",
-            StacksEpochId::Epoch23 => self.version == "3" || self.version == "4",
-            StacksEpochId::Epoch24 => self.version == "3" || self.version == "4",
+            StacksEpochId::Epoch20 => self.version == "1" || self.version == "2",
+            StacksEpochId::Epoch2_05 => self.version == "2",
+            StacksEpochId::Epoch21 => self.version == "2",
+            StacksEpochId::Epoch22 => self.version == "2",
+            StacksEpochId::Epoch23 => self.version == "2",
+            StacksEpochId::Epoch24 => self.version == "2",
         }
     }
 }
@@ -965,13 +958,6 @@ impl StacksChainState {
                         }
                     }
                     "2" => {
-                        // migrate to 3
-                        info!("Migrating chainstate schema from version 2 to 3");
-                        for cmd in CHAINSTATE_SCHEMA_3.iter() {
-                            tx.execute_batch(cmd)?;
-                        }
-                    }
-                    "3" => {
                         // done
                         break;
                     }
